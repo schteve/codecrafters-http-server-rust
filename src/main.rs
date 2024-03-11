60e018d859246d94b9380461690ddaacb95ee54d
Steve Heindel <stevenheindel@gmail.com>
2024-03-11 00:23:48 -0400

    Stage 2 - Respond with 200

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
-use std::net::TcpListener;
+use std::{io::{Error, Read, Write}, net::{TcpListener, TcpStream}};
+
+fn handle_conn(stream: TcpStream) -> Result<(), Error> {
+    let mut stream = stream;
+
+    let mut buf = [0u8; 1024];
+    let _bytes_read = stream.read(&mut buf)?;
+
+    let _bytes_write = stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
+
+    Ok(())
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+    for conn in listener.incoming() {
+        match conn {
+            Ok(stream) => {
+                println!("Accepted new connection");
+                match handle_conn(stream) {
+                    Ok(_) => (),
+                    Err(e) => println!("Error handling connection: {e}"),
+                }
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("Error accepting new connection: {}", e);
             }
         }
     }
