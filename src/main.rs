c67fa8a978c01be5a842082bc589a610a4f3b69d
Steve Heindel <stevenheindel@gmail.com>
2024-03-12 21:02:55 -0400

    Stage 6 - Concurrent connections

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use std::{
-    io::{Read, Write},
+use std::str::from_utf8;
+
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
-    str::from_utf8,
 };
 
 use http_server_starter_rust::http;
 
-fn handle_conn(stream: TcpStream) -> anyhow::Result<()> {
+async fn handle_conn(stream: TcpStream) -> anyhow::Result<()> {
     let mut stream = stream;
 
     let mut buf = [0u8; 1024];
-    let bytes_read = stream.read(&mut buf)?;
+    let bytes_read = stream.read(&mut buf).await?;
     let buf_str = from_utf8(&buf[0..bytes_read])?;
 
     let (_, req) = http::Request::parser(buf_str).map_err(|err| err.map(|e| e.input.to_owned()))?;
@@ -27,7 +28,7 @@ fn handle_conn(stream: TcpStream) -> anyhow::Result<()> {
         let user_agent = req
             .headers
             .get("user-agent")
-            .map_or_else(|| String::new(), |ua| ua.clone());
+            .map_or_else(String::new, |ua| ua.clone());
         println!("  GET user-agent - {user_agent}");
         http::Response::new()
             .with_status(http::Status::Ok)
@@ -36,26 +37,27 @@ fn handle_conn(stream: TcpStream) -> anyhow::Result<()> {
         println!("  GET unknown - 404");
         http::Response::new().with_status(http::Status::NotFound)
     };
-    let _bytes_write = stream.write(response.to_string().as_bytes())?;
+    let _bytes_write = stream.write(response.to_string().as_bytes()).await?;
 
     Ok(())
 }
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:4221").await.unwrap();
 
-    for conn in listener.incoming() {
-        match conn {
-            Ok(stream) => {
+    loop {
+        match listener.accept().await {
+            Ok((stream, _)) => {
                 println!("Accepted new connection");
-                match handle_conn(stream) {
-                    Ok(_) => (),
-                    Err(e) => println!("Error handling connection: {e}"),
-                }
+                tokio::spawn(async move {
+                    match handle_conn(stream).await {
+                        Ok(_) => println!("Connection handled successfully"),
+                        Err(e) => println!("Error handling connection: {e}"),
+                    }
+                });
             }
-            Err(e) => {
-                println!("Error accepting new connection: {}", e);
-            }
+            Err(e) => println!("Failed to accept new connection: {e}"),
         }
     }
 }
